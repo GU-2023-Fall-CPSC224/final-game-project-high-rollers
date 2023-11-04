e815d94bd446f675d35fa879347a9bdb7c5999d8
Cooper Stepanian <cstepanian@zagmail.gonzaga.edu>
2023-11-04 13:30:09 -0700

    added a discard and a few more tests

--- src/test/java/edu/gonzaga/DeckTest.java
+++ src/test/java/edu/gonzaga/DeckTest.java
@@ -14,4 +14,74 @@ public class DeckTest {
         }
         assertEquals(expectedCardCount,actualCardCount);
     }
+    @Test
+    void testNumberOfClub(){
+        Deck deck = new Deck();
+        int expectedClubs = 13;
+        String club = "club";
+        int actual = 0;
+        for(Card card : deck.cards){
+            if(card.getSuit().equals(club)){
+                actual++;
+            }
+        }
+        assertEquals(expectedClubs,actual);
+    }
+    @Test
+    void testNumberOfHeart(){
+        Deck deck = new Deck();
+        int expectedClubs = 13;
+        String club = "heart";
+        int actual = 0;
+        for(Card card : deck.cards){
+            if(card.getSuit().equals(club)){
+                actual++;
+            }
+        }
+        assertEquals(expectedClubs,actual);
+    }
+    @Test
+    void testNumberOfDiamond(){
+        Deck deck = new Deck();
+        int expectedClubs = 13;
+        String club = "diamond";
+        int actual = 0;
+        for(Card card : deck.cards){
+            if(card.getSuit().equals(club)){
+                actual++;
+            }
+        }
+        assertEquals(expectedClubs,actual);
+    }
+    @Test
+    void testNumberOfSpade(){
+        Deck deck = new Deck();
+        int expectedClubs = 13;
+        String club = "spade";
+        int actual = 0;
+        for(Card card : deck.cards){
+            if(card.getSuit().equals(club)){
+                actual++;
+            }
+        }
+        assertEquals(expectedClubs,actual);
+    }
+    @Test
+    void testCardNumberGivePlayer(){
+        Deck deck = new Deck();
+        int expectedAvailable = 42;
+        for(int i = 0; i < 10; i++){
+            deck.givePlayerCard();
+        }
+        assertEquals(expectedAvailable,deck.availableCards);
+    }
+    @Test
+    void testCardNumberGiveDealer(){
+        Deck deck = new Deck();
+        int expectedAvailable = 42;
+        for(int i = 0; i < 10; i++){
+            deck.giveDealerCard();
+        }
+        assertEquals(expectedAvailable,deck.availableCards);
+    }
 }
