bc914ac1be900a933f294ca581b59dc51ea31002
Cooper Stepanian <cstepanian@zagmail.gonzaga.edu>
2023-12-02 15:37:38 -0800

    changed background for starting screen. also added the button to just be the start.png. changed height of frame to better fit the png but going to update the png to keep same frame size

--- src/main/java/edu/gonzaga/BlackJack.java
+++ src/main/java/edu/gonzaga/BlackJack.java
@@ -13,7 +13,7 @@ public class BlackJack{
     Round round;
     Deck deck;
     boolean roundOver = false;
-    ImageIcon turnedOverCard;
+    ImageIcon background;
 
     int hitCount = 1;
     Card playerCardNum1;
@@ -37,12 +37,10 @@ public class BlackJack{
 
     JLabel playerText = new JLabel("Player Cards");
     JLabel dealerText = new JLabel("Dealer Cards");
-
+    ImageIcon icon = new ImageIcon("PNG-cards-1.3/start.png");
+    JButton startButton  = new JButton(icon);
     // starting Screen
-    JButton startButton  = new JButton("Start");
-    JLabel startingCard = new JLabel();
-    JLabel startingText = new JLabel("Welcome to Java Blackjack");
-    JLabel groupNameText = new JLabel("Designed by: Harrison, Cooper, Asher");
+    JLabel backgroundScreen = new JLabel();
 
     public static void main(String [] args){
         BlackJack app = new BlackJack();
@@ -107,14 +105,15 @@ public class BlackJack{
     }
 
     void startingScreenGUI(){
-
         this.startingScreenFrame = new JFrame("Black Jack");
         this.startingScreenFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.startingScreenFrame.setLocation(100,100);
         this.startingScreenPanel = genStartingScreenPanel();
         startingScreenFrame.setResizable(false);
         addButtonCallbackHandlers();
+        new Background();
     }
+
     void blackJackGUI(){
 
         this.blackJackScreenFrame = new JFrame("Black Jack");
@@ -131,25 +130,21 @@ public class BlackJack{
     private JPanel genStartingScreenPanel(){
 
         JPanel newPanel = new JPanel();
-        setStartingTextStyle();
         newPanel.setLayout(null);
 
-        turnedOverCard = new ImageIcon(new ImageIcon("PNG-cards-1.3/card back red.png").getImage().getScaledInstance(150,150,Image.SCALE_SMOOTH));
-        startingCard.setIcon(turnedOverCard);
+        background = new ImageIcon(new ImageIcon("PNG-cards-1.3/intro.png").getImage().getScaledInstance(700,500,Image.SCALE_SMOOTH));
+        backgroundScreen.setIcon(background);
+
 
         newPanel.add(startButton);
-        newPanel.add(startingCard);
-        newPanel.add(startingText);
-        newPanel.add(groupNameText);
-        newPanel.add(startingCard);
+        newPanel.add(backgroundScreen);
+        newPanel.add(backgroundScreen);
 
-        startButton.setBounds(300,400,100,25);
-        startingText.setBounds(250,0 , 500,50);
-        groupNameText.setBounds(200,28, 500,100);
-        startingCard.setBounds(300,150, 150,150);
+        startButton.setBounds(300,236,100,25);
+        backgroundScreen.setBounds(0,0, 700,500);
 
         startingScreenFrame.add(newPanel);
-        startingScreenFrame.setSize(700,550);
+        startingScreenFrame.setSize(700,525);
         startingScreenFrame.setVisible(true);
         startingScreenFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
         newPanel.setBackground(new Color(35,54,5));
@@ -227,21 +222,6 @@ public class BlackJack{
         blackJackScreenFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
 
         return newPanel;
-    }
-
-    public void setStartingTextStyle(){
-        startingText.setHorizontalTextPosition(JLabel.CENTER); // set text according to JLabel ( Left, center, or right)
-        startingText.setVerticalTextPosition(JLabel.BOTTOM);
-        startingText.setFont(new Font("MV Boli", Font.ITALIC, 17)); // set font of text
-        startingText.setIconTextGap(10); // set gap of text to image either plus or minus
-
-        groupNameText.setHorizontalTextPosition(JLabel.CENTER); // set text according to JLabel ( Left, center, or right)
-        groupNameText.setVerticalTextPosition(JLabel.BOTTOM);
-        groupNameText.setFont(new Font("MV Boli", Font.ITALIC, 17)); // set font of text
-        groupNameText.setIconTextGap(10); // set gap of text to image either plus or minus
-
-        groupNameText.setForeground(new Color(227, 217, 217));
-        startingText.setForeground(new Color(227, 217, 217));
     }
 
     public void dealerTurn() {
